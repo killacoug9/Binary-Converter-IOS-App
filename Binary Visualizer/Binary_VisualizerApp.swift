726ebe6d8fbc667f8dd5817ba37264a2ba6b789d
Kyle Hawkins <cougkyle9@hotmail.com>
2022-08-14 21:28:32 -0700

    Initial Commit

---
+++ Binary Visualizer/Binary_VisualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Binary_VisualizerApp.swift
+//  Binary Visualizer
+//
+//  Created by Kyle Hawkins on 8/14/22.
+//
+
+import SwiftUI
+
+@main
+struct Binary_VisualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Binary Visualizer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Binary Visualizer
+//
+//  Created by Kyle Hawkins on 8/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
